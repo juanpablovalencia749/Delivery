50b4229f25616b2a786b9be3e0e177b1423c264f
Juan Pablo Valencia <juanpablovalencia749@gmail.com>
2022-11-26 17:12:00 -0500

    Initiation

---
+++ Project/src/components/Form.vue
@@ -0,0 +1,49 @@
+
+
+<template>
+  <section class="main">
+    <div class="main__contact">
+      <h2 class="main__title">Hello Again</h2>
+      <p class="main__paragraph">Welcome back you,ve been missed</p>  
+
+      
+
+      <form action="" class="main__form">
+
+        <input type="text" name="" id="" placeholder="origen" class="main__input">
+
+        <input type="text" name="" id="" placeholder="skydrop" class="main__input">
+
+        <input type="text" name="" id="" placeholder="skydrop" class="main__input">
+
+        <input type="text" name="" id="" placeholder="Correo electronico" class="">
+
+        <input type="text" name="" id="" placeholder="dirrecion" class="main__input">
+
+        <input type="text" name="" id="" placeholder="Numero Interior" class="main__input">
+
+        <input type="text" name="" id="" placeholder="ciudad" class="main__input">
+
+        <input type="text" name="" id="" placeholder="pais" class="main__input">
+
+        <input type="text" name="" id="" placeholder="detalles" class="main__input">
+        
+
+      </form>
+    </div>
+
+  </section>
+</template>
+
+<style>
+
+*{
+  margin:0%;
+  box-sizing:border-box;
+  
+}
+
+
+
+ 
+</style>
